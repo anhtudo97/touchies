8e10cdc423912cf5d9f357ff7e2ca2858334f1cd
Alex Dou <anhtudo97@gmail.com>
2026-05-22 02:15:32 +0700

    feat: update convex with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
-import { ThemeProvider } from "@/components/theme-provider";
-import { ClerkProvider } from '@clerk/nextjs';
-import { dark } from "@clerk/themes";
+
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 
+import { Providers } from "@/components/provider";
 import "./globals.css";
-import { ConvexClientProvider } from "@/components/provider";
 
 const inter = Inter({
   variable: "--font-inter-sans",
@@ -29,29 +27,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        theme: dark
-      }}
+    <html
+      lang="en"
+      className={`${inter.variable} ${plexMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <ConvexClientProvider>
-        <html
-          lang="en"
-          className={`${inter.variable} ${plexMono.variable} h-full antialiased`}
-          suppressHydrationWarning
-        >
-          <body className="min-h-full flex flex-col">
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-            >
-              {children}
-            </ThemeProvider>
-          </body>
-        </html>
-      </ConvexClientProvider>
-    </ClerkProvider>
+      <body className="min-h-full flex flex-col">
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
   );
 }
--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,10 +1,31 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ReactNode } from "react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { PropsWithChildren } from "react";
+import { ThemeProvider } from "./theme-provider";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+export function Providers({ children }: PropsWithChildren) {
+  return (
+    <ClerkProvider
+      appearance={{
+        theme: dark
+      }}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
