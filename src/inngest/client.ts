0b5068564b17efda31a7aba5664be57ae12cbd6c
Alex Dou <anhtudo97@gmail.com>
2026-05-26 02:02:22 +0700

    feat: apply log sentry

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,5 +1,7 @@
+import { sentryMiddleware } from '@inngest/middleware-sentry';
 import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({
     id: "touchies",
-})
+    middleware: [sentryMiddleware()]
+});
