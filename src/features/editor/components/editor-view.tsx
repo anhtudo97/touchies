1f3345eb472242d0c293731ab32425fc89a2326a
Alex Dou <anhtudo97@gmail.com>
2026-06-09 01:57:03 +0700

    feat: editor navigation

---
+++ src/features/editor/components/editor-view.tsx
@@ -0,0 +1,13 @@
+import { Id } from "../../../../convex/_generated/dataModel";
+import { TopNavigation } from "./top-navigator";
+
+
+export const EditorView = ({ projectId }: { projectId: Id<"projects">; }) => {
+    return (
+        <div className="h-full flex flex-col">
+            <div className="flex items-center">
+                <TopNavigation projectId={projectId} />
+            </div>
+        </div>
+    );
+};
